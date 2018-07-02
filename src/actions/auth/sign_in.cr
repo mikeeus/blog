f81941cec3a48e7a1925fbf8bc8acafe753ea996
mikeeus <itsmikias@gmail.com>
2018-07-02 17:19:54 +0300

    Refactor generate_token method

--- src/actions/auth/sign_in.cr
+++ src/actions/auth/sign_in.cr
@@ -1,26 +1,16 @@
 require "jwt"
 
 class Auth::SignIn < ApiAction
+  include GenerateToken
+
   post "/auth/sign_in" do
     SignInForm.new(params).submit do |form, user|
       if user
-        context.response.headers.add "Authorization", "Bearer #{token(user)}"
+        context.response.headers.add "Authorization", "Bearer #{generate_token(user)}"
         head 200
       else
-        pp form
         head 401
       end
     end
-  end
-
-  private def token(user : User)
-    exp = Time.now.epoch + 60 * 60
-    sub = ({ id: user.id, name: user.name, email: user.email }).to_s
-    payload = {
-      "sub" => Base64.encode(sub),
-      "exp" => exp
-    }
-
-    JWT.encode(payload, Lucky::Server.settings.secret_key_base, "HS256")
   end
 end
---
+++ src/actions/mixins/generate_token.cr
@@ -0,0 +1,9 @@
+module GenerateToken
+  private def generate_token(user : User)
+    exp = Time.now.epoch + 60 * 60
+    sub = ({ id: user.id, name: user.name, email: user.email }).to_s
+    payload = { "sub" => Base64.encode(sub), "exp" => exp }
+
+    JWT.encode(payload, Lucky::Server.settings.secret_key_base, "HS256")
+  end
+end
