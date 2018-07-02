640e7a1568352bf7864b339572893de5a42807e9
mikeeus <itsmikias@gmail.com>
2018-07-02 19:49:48 +0300

    Add from_token method

--- src/queries/user_query.cr
+++ src/queries/user_query.cr
@@ -1,2 +1,11 @@
 class UserQuery < User::BaseQuery
+
+  def from_token_payload(payload)
+    find(payload["sub"].to_s)
+  end
+
+  def from_token(token : String)
+    payload, _header = JWT.decode(token, Lucky::Server.settings.secret_key_base, "HS256")
+    from_token_payload(payload)
+  end
 end
